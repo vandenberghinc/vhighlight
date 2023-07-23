7c6aae388aef975ecf11a8adb010cff5492a4689
Daan van den Bergh <vandenberghinc@gmail.com>
2023-07-23 10:37:40 +0200

    Auto commit.

--- include/vhighlight/js/highlight.js
+++ include/vhighlight/js/highlight.js
@@ -10,45 +10,212 @@ const vhighlight = {};
 	
 // Highlight
 // Make sure to replace < with &lt; and > with &gt; before assigning the code to the <code> element.
-vhighlight.highlight = function(element, options = {}) {
+vhighlight.highlight = function({
+	element = null,			// the html code element.
+	language = null,		// code language, precedes element attribute "language".
+	line_numbers = null,	// show line numbers, precedes element attribute "line_numbers".
+	animate = false,		// animate code writing.
+	delay = 25,				// animation delay in milliseconds.
+	is_func = false	 		// enable when cpp code is inside a function.
+}) {
 
 	// Get language.
-	const language = element.getAttribute("language");
-	if (language == "") {
+	if (language == null) {
+		language = element.getAttribute("language");
+	}
+	if (language == "" || language == null) {
 		return ;
 	}
-
-	// Get code.
-	const code = element.textContent;
-	element.innerHTML = "";
-
-	// Set loader.
-	element.style.display = 'flex';
-	element.style.justifyContent = "center";
-	element.style.alignItems = "center";
-	let loader = document.createElement("div");
-	loader.className = "vhighlight_loader";
-	for (let i = 0; i < 4; i++) {
-		let child = document.createElement("div");
-		child.style.border = "4px solid " + element.style.color;
-		child.style.borderColor = element.style.color + " transparent transparent transparent";
-		loader.appendChild(child);
+	
+	// Line numbers.
+	if (line_numbers == null) {
+		line_numbers = element.getAttribute('line_numbers') == "true";
 	}
-	element.appendChild(loader);
+	
+	// Delay.
+	if (delay == null) {
+		delay = 25;
+	}
+	
+	// Create children.
+	let code;
+	let loader;
+	let line_numbers_div;
+	let line_numbers_divider;
+	let code_pre;
+	if (element.children.length <= 1) {
+		
+		// Set style.
+		element.style.display = 'flex';
+		element.style.height = "auto";
+		if (element.style.fontFamily == "") {
+			element.style.fontFamily = "'Menlo', 'Consolas', monospace";
+		}
+		
+		// Get code.
+		code = element.textContent;
+		element.innerHTML = "";
+		
+		// Loader.
+		loader = document.createElement("div");
+		loader.style.display = "flex";
+		loader.className = "vhighlight_loader";
+		for (let i = 0; i < 4; i++) {
+			let child = document.createElement("div");
+			child.style.border = "4px solid " + element.style.color;
+			child.style.borderColor = element.style.color + " transparent transparent transparent";
+			loader.appendChild(child);
+		}
+		element.appendChild(loader);
+		
+		// Line numbers.
+		line_numbers_div = document.createElement("pre");
+		line_numbers_div.style.padding = '0px';
+		line_numbers_div.style.margin = '0px';
+		element.appendChild(line_numbers_div);
+		
+		// Line numbers divider.
+		line_numbers_divider = document.createElement("div");
+		line_numbers_divider.className = "token_line_number_divider";
+		line_numbers_divider.style.minWidth = "0.5px";
+		line_numbers_divider.style.width = "0.5px";
+		line_numbers_divider.style.padding = '0px';
+		line_numbers_divider.style.margin = "0px 10px 0px 10px";
+		element.appendChild(line_numbers_divider);
+		
+		// Code.
+		code_pre = document.createElement("pre");
+		code_pre.style.padding = "0px";
+		code_pre.style.margin = "0px";
+		code_pre.style.whiteSpace = "pre";
+		code_pre.style.overflowX = "auto";
+		element.appendChild(code_pre);
+	}
+	
+	// Get elements.
+	else {
+		console.log(element.children);
+		loader = element.children[0];
+		line_numbers_div = element.children[1];
+		line_numbers_divider = element.children[2];
+		code_pre = element.children[3];
+		code = code_pre.textContent;
+		console.log("CODE PRE: ", code_pre);
+		console.log("CODE: ", code);
+	}
+
+	// Functions.
+	function show_loader() {
+		element.style.justifyContent = "center";
+		element.style.alignItems = "center";
+		loader.style.display = "flex";
+		line_numbers_div.style.display = "none";
+		line_numbers_divider.style.display = "none";
+		code_pre.style.display = "none";
+	}
+	function hide_loader() {
+		element.style.justifyContent = "start";
+		element.style.alignItems = "stretch";
+		loader.style.display = "none";
+		if (line_numbers) {
+			line_numbers_div.style.display = "block";
+			line_numbers_divider.style.display = "block";
+		}
+		code_pre.style.display = "block";
+	}
+	function animate_writing(highlighted_code) {
+		code_pre.innerHTML = "";
+		
+		// Add char.
+		function add_char(index) {
+			if (index < highlighted_code.length) {
+				
+				// Span opening.
+				if (highlighted_code[index] == '<') {
+					
+					// Fins span open, close and code.
+					let span_index;
+					let span_open = "";
+					let span_close = "";
+					let span_code = "";
+					let open = true;
+					let first = true;
+					for (span_index = index; span_index < highlighted_code.length; span_index++) {
+						const char = highlighted_code[span_index];
+						if (char == '<' || open) {
+							open = true;
+							if (first) {
+								span_open += char;
+							} else {
+								span_close += char;
+							}
+							if (char == '>') {
+								open = false;
+								if (first) {
+									first = false;
+									continue;
+								}
+									
+								// Animate span code writing.
+								let before = code_pre.innerHTML;
+								let added_span_code = "";
+								function add_span_code(index) {
+									if (index < span_code.length) {
+										added_span_code += span_code[index]
+										let add = before;
+										add += span_open;
+										add += added_span_code;
+										add += span_close;
+										code_pre.innerHTML = add;
+										setTimeout(() => add_span_code(index + 1), delay);
+									} else {
+										setTimeout(() => add_char(span_index + 1), delay);
+									}
+								}
+								add_span_code(0)
+								
+								// Stop.
+								break;
+							}
+						}
+						
+						// Add non span code.
+						else {
+							span_code += char;
+						}
+						
+					}
+				}
+				
+				// Non span code.
+				else {
+					code_pre.innerHTML += highlighted_code.charAt(index);
+					setTimeout(() => add_char(index + 1), delay);
+				}
+			}
+			
+			// Non span code.
+			else {
+				code_pre.innerHTML = highlighted_code;
+			}
+			
+		}
+		
+		// Start animation.
+		add_char(0);
+	}
+	
+	// Show loader.
+	show_loader();
 
 	// Delay the syntax highlighting process.
 	// Otherwise the loader does not show and the unhighlted code is shown instead.
 	setTimeout(() => {
 
-		// Set style.
-		if (element.style.fontFamily == "") {
-			element.style.fontFamily = "'Menlo', 'Consolas', monospace";
-		}
-
 		// Highlight.
 		let highlighted_code;
 		if (language == "cpp" || language == "c++" || language == "c") {
-			highlighted_code = vhighlight.cpp.highlight(code, options);
+			highlighted_code = vhighlight.cpp.highlight(code, {is_func: is_func});
 		} else if (language == "markdown" || language == "md") {
 			highlighted_code = vhighlight.md.highlight(code);
 		} else if (language == "js" || language == "javascript") {
@@ -64,21 +231,19 @@ vhighlight.highlight = function(element, options = {}) {
 		}
 
 		// No line numbers.
-		if (element.getAttribute('line_numbers') != "true") {
-			let code_pre = document.createElement("pre");
-			code_pre.style.padding = "0px";
-			code_pre.style.margin = "0px";
-			code_pre.style.whiteSpace = "pre";
-			code_pre.style.overflowX = "auto";
-			code_pre.innerHTML = highlighted_code;
-			element.style.justifyContent = "start";
-			element.style.alignItems = "stretch";
-			element.innerHTML = "";
-			element.appendChild(code_pre);
+		// So add code directly.
+		if (line_numbers == false) {
+			hide_loader();
+			if (animate == true) {
+				animate_writing(highlighted_code);
+			} else {
+				code_pre.innerHTML = highlighted_code;
+			}
 			return ;
 		}
 		
 		// Set style for line numbers.
+		element.style.justifyContent = "start";
 		element.style.alignItems = 'stretch';
 		if (element.style.height === 'undefined' || element.style.height == "100%") {
 			element.style.height = 'auto';
@@ -103,44 +268,24 @@ vhighlight.highlight = function(element, options = {}) {
         document.body.removeChild(pre);
         const lines = Math.floor(pre_height / line_height);
 
-        let line_div = document.createElement("div");
-        line_div.style.display = "flex";
-        line_div.style.padding = '0px';
-        line_div.style.margin = '0px';
-
-        let line_numbers = document.createElement("pre");
-        line_numbers.style.padding = '0px';
-        line_numbers.style.margin = '0px';
+		// Set line numbers.
+        line_numbers_div.innerHTML = "";
 		for (var i = 0; i < lines; i++) {
 			let span = document.createElement("span");
 			span.className = "token_line_number";
 			span.textContent = (i + 1) + "\n";
-			line_numbers.appendChild(span);
+			line_numbers_div.appendChild(span);
 		}
 
-		let divider = document.createElement("div");
-		divider.className = "token_line_number_divider";
-		divider.style.minWidth = "0.5px";
-		divider.style.width = "0.5px";
-		divider.style.padding = '0px';
-		divider.style.margin = "0px 10px 0px 10px";
-
-		let code_pre = document.createElement("pre");
-		code_pre.style.padding = "0px";
-		code_pre.style.margin = "0px";
-		code_pre.style.whiteSpace = "pre";
-		code_pre.style.overflowX = "auto";
-
-		code_pre.innerHTML = highlighted_code;
-		element.style.height = "auto";
-		element.style.justifyContent = "start";
-		element.style.alignItems = "stretch";
-		element.innerHTML = "";
-
-		element.appendChild(line_numbers);
-		element.appendChild(divider);
-		element.appendChild(code_pre);
-	
+		// Set code.
+		hide_loader();
+		if (animate == true) {
+			animate_writing(highlighted_code);
+		} else {
+			code_pre.innerHTML = highlighted_code;
+		}
+		
+		
 	}, 50);
 }
 
--- include/vhighlight/js/vhighlight.js
+++ include/vhighlight/js/vhighlight.js
@@ -3,17 +3,35 @@
  * Copyright: © 2022 - 2023 Daan van den Bergh.
  */
 const vhighlight={};
-vhighlight.highlight=function(element,options={}){
-const language=element.getAttribute("language");
-if(language==""){
+vhighlight.highlight=function({
+element=null,language=null,line_numbers=null,animate=false,delay=25,is_func=false	}){
+if(language==null){
+language=element.getAttribute("language");
+}
+if(language==""||language==null){
 return;
 }
-const code=element.textContent;
-element.innerHTML="";
+if(line_numbers==null){
+line_numbers=element.getAttribute('line_numbers')=="true";
+}
+if(delay==null){
+delay=25;
+}
+let code;
+let loader;
+let line_numbers_div;
+let line_numbers_divider;
+let code_pre;
+if(element.children.length<=1){
 element.style.display='flex';
-element.style.justifyContent="center";
-element.style.alignItems="center";
-let loader=document.createElement("div");
+element.style.height="auto";
+if(element.style.fontFamily==""){
+element.style.fontFamily="'Menlo', 'Consolas', monospace";
+}
+code=element.textContent;
+element.innerHTML="";
+loader=document.createElement("div");
+loader.style.display="flex";
 loader.className="vhighlight_loader";
 for(let i=0;i<4;i++){
 let child=document.createElement("div");
@@ -22,13 +40,118 @@ child.style.borderColor=element.style.color+" transparent transparent transparen
 loader.appendChild(child);
 }
 element.appendChild(loader);
-setTimeout(()=>{
-if(element.style.fontFamily==""){
-element.style.fontFamily="'Menlo', 'Consolas', monospace";
+line_numbers_div=document.createElement("pre");
+line_numbers_div.style.padding='0px';
+line_numbers_div.style.margin='0px';
+element.appendChild(line_numbers_div);
+line_numbers_divider=document.createElement("div");
+line_numbers_divider.className="token_line_number_divider";
+line_numbers_divider.style.minWidth="0.5px";
+line_numbers_divider.style.width="0.5px";
+line_numbers_divider.style.padding='0px';
+line_numbers_divider.style.margin="0px 10px 0px 10px";
+element.appendChild(line_numbers_divider);
+code_pre=document.createElement("pre");
+code_pre.style.padding="0px";
+code_pre.style.margin="0px";
+code_pre.style.whiteSpace="pre";
+code_pre.style.overflowX="auto";
+element.appendChild(code_pre);
 }
+else{
+console.log(element.children);
+loader=element.children[0];
+line_numbers_div=element.children[1];
+line_numbers_divider=element.children[2];
+code_pre=element.children[3];
+code=code_pre.textContent;
+console.log("CODE PRE: ",code_pre);
+console.log("CODE: ",code);
+}
+function show_loader(){
+element.style.justifyContent="center";
+element.style.alignItems="center";
+loader.style.display="flex";
+line_numbers_div.style.display="none";
+line_numbers_divider.style.display="none";
+code_pre.style.display="none";
+}
+function hide_loader(){
+element.style.justifyContent="start";
+element.style.alignItems="stretch";
+loader.style.display="none";
+if(line_numbers){
+line_numbers_div.style.display="block";
+line_numbers_divider.style.display="block";
+}
+code_pre.style.display="block";
+}
+function animate_writing(highlighted_code){
+code_pre.innerHTML="";
+function add_char(index){
+if(index<highlighted_code.length){
+if(highlighted_code[index]=='<'){
+let span_index;
+let span_open="";
+let span_close="";
+let span_code="";
+let open=true;
+let first=true;
+for(span_index=index;span_index<highlighted_code.length;span_index++){
+const char=highlighted_code[span_index];
+if(char=='<'||open){
+open=true;
+if(first){
+span_open+=char;
+}else{
+span_close+=char;
+}
+if(char=='>'){
+open=false;
+if(first){
+first=false;
+continue;
+}
+let before=code_pre.innerHTML;
+let added_span_code="";
+function add_span_code(index){
+if(index<span_code.length){
+added_span_code+=span_code[index]
+let add=before;
+add+=span_open;
+add+=added_span_code;
+add+=span_close;
+code_pre.innerHTML=add;
+setTimeout(()=>add_span_code(index+1),delay);
+}else{
+setTimeout(()=>add_char(span_index+1),delay);
+}
+}
+add_span_code(0)
+break;
+}
+}
+else{
+span_code+=char;
+}
+}
+}
+else{
+code_pre.innerHTML+=highlighted_code.charAt(index);
+setTimeout(()=>add_char(index+1),delay);
+}
+}
+else{
+code_pre.innerHTML=highlighted_code;
+}
+}
+add_char(0);
+}
+show_loader();
+setTimeout(()=>{
 let highlighted_code;
 if(language=="cpp"||language=="c++"||language=="c"){
-highlighted_code=vhighlight.cpp.highlight(code,options);
+highlighted_code=vhighlight.cpp.highlight(code,{is_func:is_func});
 }else if(language=="markdown"||language=="md"){
 highlighted_code=vhighlight.md.highlight(code);
 }else if(language=="js"||language=="javascript"){
@@ -41,19 +164,16 @@ highlighted_code=vhighlight.python.highlight(code);
 console.error("Unsupported language \""+language+"\" for syntax highlighting.");
 return;
 }
-if(element.getAttribute('line_numbers')!="true"){
-let code_pre=document.createElement("pre");
-code_pre.style.padding="0px";
-code_pre.style.margin="0px";
-code_pre.style.whiteSpace="pre";
-code_pre.style.overflowX="auto";
+if(line_numbers==false){
+hide_loader();
+if(animate==true){
+animate_writing(highlighted_code);
+}else{
 code_pre.innerHTML=highlighted_code;
-element.style.justifyContent="start";
-element.style.alignItems="stretch";
-element.innerHTML="";
-element.appendChild(code_pre);
+}
 return;
 }
+element.style.justifyContent="start";
 element.style.alignItems='stretch';
 if(element.style.height==='undefined'||element.style.height=="100%"){
 element.style.height='auto';
@@ -73,38 +193,19 @@ const pre_height=pre.clientHeight;
 const line_height=parseFloat(element.style.lineHeight);
 document.body.removeChild(pre);
 const lines=Math.floor(pre_height/line_height);
-let line_div=document.createElement("div");
-line_div.style.display="flex";
-line_div.style.padding='0px';
-line_div.style.margin='0px';
-let line_numbers=document.createElement("pre");
-line_numbers.style.padding='0px';
-line_numbers.style.margin='0px';
+line_numbers_div.innerHTML="";
 for(var i=0;i<lines;i++){
 let span=document.createElement("span");
 span.className="token_line_number";
 span.textContent=(i+1)+"\n";
-line_numbers.appendChild(span);
+line_numbers_div.appendChild(span);
 }
-let divider=document.createElement("div");
-divider.className="token_line_number_divider";
-divider.style.minWidth="0.5px";
-divider.style.width="0.5px";
-divider.style.padding='0px';
-divider.style.margin="0px 10px 0px 10px";
-let code_pre=document.createElement("pre");
-code_pre.style.padding="0px";
-code_pre.style.margin="0px";
-code_pre.style.whiteSpace="pre";
-code_pre.style.overflowX="auto";
+hide_loader();
+if(animate==true){
+animate_writing(highlighted_code);
+}else{
 code_pre.innerHTML=highlighted_code;
-element.style.height="auto";
-element.style.justifyContent="start";
-element.style.alignItems="stretch";
-element.innerHTML="";
-element.appendChild(line_numbers);
-element.appendChild(divider);
-element.appendChild(code_pre);
+}
 },50);
 }
 vhighlight.js={};
@@ -352,7 +453,7 @@ vhighlight.cpp.call_regex=new RegExp(`${vhighlight.cpp.exclude_span}\\b([A-Za-z0
 vhighlight.cpp.type_regex=new RegExp(`${vhighlight.cpp.exclude_span}${vhighlight.cpp.html_open}(?<=\\b)(${vhighlight.cpp.keywords.join('\\s+|') + '\\s+'})*([A-Za-z0-9_]+${vhighlight.cpp.template_params})([&*]*\\s+(?!&gt;|&lt;)[A-Za-z0-9_&*]+)${vhighlight.cpp.html_close}`,'gm');
 vhighlight.cpp.namespace_regex=new RegExp(`${vhighlight.cpp.exclude_span}([A-Za-z0-9_]*)(::)([A-Za-z0-9_]+${vhighlight.cpp.template_params})`,'g');
 vhighlight.cpp.recorrect_regex=/(<span class="token_type">[^$2|<]*)(,|::|&lt;|&gt;)([^<]*<\/span>)/g;
-vhighlight.cpp.highlight=function(code,options={is_func: false,reformat: true}){
+vhighlight.cpp.highlight=function(code,options={is_func:false,reformat:true}){
 if(options.reformat){code=code.replaceAll("<","&lt;");
 code=code.replaceAll(">","&gt;");
 }
@@ -450,7 +551,7 @@ highlighted+="</span>";
 highlighted+=vhighlight.cpp.highlight_params(func_params);
 if(bend!=0){
 highlighted+=" ";
-highlighted+=this.highlight(func_code,{is_func: true,reformat: false});
+highlighted+=this.highlight(func_code,{is_func:true,reformat:false});
 }else{
 highlighted+=";";
 }
@@ -599,9 +700,9 @@ end=index;
 }
 if(start==end){return null;}
 return{
-"start": start,
-"end": end,
-"data": code.substr(start,end-start),
+"start":start,
+"end":end,
+"data":code.substr(start,end-start),
 }
 }
 }
