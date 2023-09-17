53997e0cff3a991177df7cfae1c6883e9f5a240c
Daan van den Bergh <vandenberghinc@gmail.com>
2023-09-17 14:04:03 +0200

    Auto commit.

--- include/vhighlight/js/tokenizer.js
+++ include/vhighlight/js/tokenizer.js
@@ -6,7 +6,7 @@
 // The tokenizer class.
 // - Do not forget to assign attribute "code" after initializing the Tokenizer, used to avoid double copy of the code string.
 // - Parsing behaviour depends on that every word is seperated as a token, so each word boundary is a seperate token.
-// @todo highlight "@\\s+" patterns inside comments.
+// @todo highlight "@\\s+" patterns inside comments, but do not highlight escaped @ chars.
 // @todo highlight `` inside comments with a codeblock background in every language.
 class Tokenizer {
 	constructor({
